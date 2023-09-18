51eff354d05d98ad6fdb4a7bf09b80abaa24f44d
Morrison Wanja <morrisonwanja@gmail.com>
2023-09-18 17:42:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+let currentUser ;
+currentUser = "MwanaMitindo";
+let welcomeMessage;
+welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//let excitedWelcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//excitedWelcomeMessage = `Welcome to Flatbook, toUppercase${currentUser.toUpperCase()}!`;
+//excitedWelcomeMessage.toUpperCase();
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
